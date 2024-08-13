2a45588e360136d5f601150c2ff1cb4673182d02
Mateo-Lara <mateo.lara.aristizabal2602@gmail.com>
2024-08-13 16:20:55 -0500

    actualización axios

--- src/configuration/AxiosConfig.js
+++ src/configuration/AxiosConfig.js
@@ -4,7 +4,6 @@ export const axiosConfig = axios.create({
     baseURL: process.env.REACT_APP_BASE_URL
 });
 
-// Añadir un interceptor para incluir el token en cada solicitud
 axiosConfig.interceptors.request.use(config => {
     const token = localStorage.getItem('access_token');
     if (token) {
